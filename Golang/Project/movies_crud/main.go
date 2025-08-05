2c15d7ebfc4d86ed8d0197a646c788c366f2883c
unsortedkgpian <theadityakumar2810@gmail.com>
2025-08-05 23:47:40 +0530

    added some endpoints

--- Golang/Project/movies_crud/main.go
+++ Golang/Project/movies_crud/main.go
@@ -24,8 +24,52 @@ type Director struct{
 
 var movies []Movie
 
+
+func getMovies(w http.ResponseWritter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWritter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params:=mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index+1:]...)//debug
+		}
+
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWritter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item :=range movies {
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}	
+}
+
+
+
+
 func main(){
 	r:=mux.NewRouter()
 
+	// appending some movies
+	movies=append(movies, Movie{ID:"1", Isbn:"532833", Title:"Movie One", Director:&Director{Firstname:"kumar", Lastname:"divyanshu"}})
+	movies=append(movies, Movie{ID:"2", Isbn:"392832", Title:"Movie Two", Director:&Director{Firstname:"Aditya", Lastname:"Kumar"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}",getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting the server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000",r))
 }
 
