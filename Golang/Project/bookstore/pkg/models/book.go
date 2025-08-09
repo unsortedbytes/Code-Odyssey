a9533c1603b07e9c5544994cddfb6b94ca352d9a
unsortedkgpian <theadityakumar2810@gmail.com>
2025-08-09 23:26:19 +0530

    models func

---
+++ Golang/Project/bookstore/pkg/models/book.go
@@ -0,0 +1,50 @@
+package models
+
+import(
+	"github.com/jinzhu/gorm"
+	"github.com/akhil/go-bookstore/pkg/config"
+)
+
+var db *gorm.DB
+
+type Book struct{
+	gorm.model 
+	Name string `gorm:""json:"name"`
+	Author string `json:"author"`
+	Publication string 'json:"publication"'
+}
+
+func init(){
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func(b *Book) CreaterBook() *Book{
+	db.NewRecord(b)
+	db.Create(&b)
+}
+
+func (b *Book) CreateBook() *Book{
+	db.NewRecord(b)
+	db.Create(&b)
+	return
+}
+
+func GetAllBooks() []Book{
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64)(*Book, *gorm.DB){
+	var getBook Book 
+	db:=db.Where("ID=?",Id).Find(&getBook)
+	return &getBook, db 
+}
+
+func DeleteBook(ID int64) Book{
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
+}
