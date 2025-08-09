660176d1f1a1dd79ad529ba52f67f954a06665cc
unsortedkgpian <theadityakumar2810@gmail.com>
2025-08-09 23:12:38 +0530

    main.go

---
+++ Golang/Project/bookstore/cmd/main/main.go
@@ -0,0 +1,18 @@
+package main
+
+import(
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/akhil/go-bookstore/pkg/routes"
+)
+
+func main(){
+	r : mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010",r ))
+
+}
+	
