b3eb10d6db930c10b206a62fe8aae14e89b4bd7f
unsortedkgpian <theadityakumar2810@gmail.com>
2025-08-11 22:41:31 +0530

    29. interpolation

--- Rust/Boris_Paskhaver/section_2/variables-and-mutability/src/main.rs
+++ Rust/Boris_Paskhaver/section_2/variables-and-mutability/src/main.rs
@@ -3,4 +3,12 @@ fn main() {
     let apples = 50;
     let oranges = 14 +8;
     let fruits = apples + oranges;
+
+    println!("My Garden ");
+    println!("My house");
+    // {} -> intrapolate
+    println!("This year, my garden have {} apples.", apples -10);
+    //println!("This year, my garden have {apples  - 10} apples");
+    // Case study 
+    println!("This year, my garden have {} apples and {} oranges", apples, oranges);
 }
