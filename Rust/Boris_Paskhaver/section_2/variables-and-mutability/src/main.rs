2d6b5eba8060eadd1480f380a5f1b46606641a32
unsortedkgpian <theadityakumar2810@gmail.com>
2025-08-12 00:12:38 +0530

    32. Immuatable and mutable

--- Rust/Boris_Paskhaver/section_2/variables-and-mutability/src/main.rs
+++ Rust/Boris_Paskhaver/section_2/variables-and-mutability/src/main.rs
@@ -15,4 +15,13 @@ fn main() {
     println!("This year, my garden have {1} apples and {0} oranges total {1} fruits", apples, oranges);
     // This help to unrepeate the arugments 
 
+    // Immutable 
+    
+
+    let gym_reps = 10;
+    let mut gym_reps_mut = 20;
+    println!("I plan to do {gym_reps} reps");
+
+    //gym_reps = 15;
+    gym_reps_mut = 23;
 }
